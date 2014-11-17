c58638c13f2efae8a406b549b280517e283ac446
fouber <fouber.NET@gmail.com>
2014-11-17 10:16:29 +0800

    hotfix

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 var exports = module.exports = require('swig')
+;['body', 'head', 'html', 'pagelet', 'require', 'script', 'uri'].forEach(function(tag){
+  var t = require('./tags/' + tag);
+  swig.setTag(tag, t.parse, t.compile, t.ends, t.blockLevel || false);
+})
 exports.Resource = require('./lib/resource.js')
