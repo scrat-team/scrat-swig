060028e3477d974168ec39d1412ee478ebeac87f
TZ <atian25@qq.com>
2015-04-22 14:52:31 +0800

    title优化, 支持参数

--- tags/title.js
+++ tags/title.js
@@ -1,7 +1,18 @@
-var exports = module.exports;
+/**
+ * Used to update document.title
+ *
+ * @example
+ * {% title "some-title" %}
+ * {% title some_var %}
+ * {% title foo["bar"] %}
+ * {% title getTitle(pageId) %}
+ */
 
 exports.compile = function(compiler, args, content, parents, options, blockName) {
-  var code = compiler(content, parents, options, blockName);
+  var code = '';
+  if(args.length > 0){
+    code = '_output +=' + args.join('');
+  }
   return ';_output += "<title>" + _ext._resource.pageletTitle((function(){var _output="";' + code + ';return _output})()) + "</title>";';
 };
 
@@ -9,4 +20,4 @@ exports.parse = function(str, line, parser, types) {
   return true;
 };
 
-exports.ends = true;
+exports.ends = false;
---
+++ tests/tags/title.spec.js
@@ -0,0 +1,41 @@
+var swig = require('swig');
+var expect = require('expect.js');
+var sinon = require('sinon');
+
+var tagName = 'title';
+var tag = require('../../tags/' + tagName);
+swig.setTag(tagName, tag.parse, tag.compile, tag.ends, tag.blockLevel || false);
+
+describe('Tags:', function () {
+  describe(tagName, function(){
+    var spy;
+    var resourceInstance = {
+      pageletTitle: function(title){
+        return title;
+      }
+    };
+
+    beforeEach(function(){
+      spy = sinon.spy(resourceInstance, "pageletTitle");
+      swig.setExtension('_resource', resourceInstance);
+    });
+
+    it('render title', function (){
+      expect(swig.render('{% title "bar"%}')).to.equal('<title>bar</title>');
+      sinon.assert.calledWith(spy, 'bar');
+      spy.reset();
+
+      expect(swig.render('{% title foo %}', {locals: {foo: "bar"}})).to.equal('<title>bar</title>');
+      sinon.assert.calledWith(spy, 'bar');
+      spy.reset();
+
+      expect(swig.render('{% title foo.key + foo["key"] + foo[v]%}', {locals: {v: "key", foo: {"key": "bar"}}})).to.equal('<title>barbarbar</title>');
+      sinon.assert.calledWith(spy, 'barbarbar');
+      spy.reset();
+
+      expect(swig.render('{% title fn(foo) %}', {locals: {fn: function(o){ return o.key}, foo: {"key": "bar"}}})).to.equal('<title>bar</title>');
+      sinon.assert.calledWith(spy, 'bar');
+      spy.reset();
+    });
+  });
+});
