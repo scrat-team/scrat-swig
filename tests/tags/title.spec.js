408e0835771534d87ab1bf941c0579e8400724f8
TZ <atian25@qq.com>
2015-04-22 18:27:49 +0800

    优化title测试用例

--- tests/tags/title.spec.js
+++ tests/tags/title.spec.js
@@ -8,40 +8,58 @@ swig.setTag(tagName, tag.parse, tag.compile, tag.ends, tag.blockLevel || false);
 
 describe('Tags:', function () {
   describe(tagName, function(){
-    var spy;
     var resourceInstance = {
       pageletTitle: function(title){
         return title;
       }
     };
+    var spy = sinon.spy(resourceInstance, "pageletTitle");
 
     beforeEach(function(){
-      spy = sinon.spy(resourceInstance, "pageletTitle");
+      spy.reset();
       swig.setExtension('_resource', resourceInstance);
     });
 
-    it.only('render title', function (){
+    it('render title', function (){
       expect(swig.render('{% title "bar"%}')).to.equal('<title>bar</title>');
       sinon.assert.calledWith(spy, 'bar');
       spy.reset();
 
+      expect(swig.render('{% title foo %}', {locals: {foo: "bar"}})).to.equal('<title>bar</title>');
+      sinon.assert.calledWith(spy, 'bar');
+      spy.reset();
+    });
+
+    it('render empty', function(){
       expect(swig.render('{% title %}', {locals: {title: 'bar'}})).to.equal('<title>bar</title>');
       sinon.assert.calledWith(spy, 'bar');
       spy.reset();
 
       expect(swig.render('{% title %}')).to.equal('<title></title>');
       spy.reset();
+    });
 
-      expect(swig.render('{% title foo %}', {locals: {foo: "bar"}})).to.equal('<title>bar</title>');
-      sinon.assert.calledWith(spy, 'bar');
-      spy.reset();
-
+    it('render fn', function(){
       expect(swig.render('{% title foo.key + foo["key"] + foo[v]%}', {locals: {v: "key", foo: {"key": "bar"}}})).to.equal('<title>barbarbar</title>');
       sinon.assert.calledWith(spy, 'barbarbar');
       spy.reset();
 
       expect(swig.render('{% title fn(foo) %}', {locals: {fn: function(o){ return o.key}, foo: {"key": "bar"}}})).to.equal('<title>bar</title>');
       sinon.assert.calledWith(spy, 'bar');
+      spy.reset();
+    });
+
+    it('render escape', function(){
+      expect(swig.render("{% title 'bar'%}")).to.equal('<title>bar</title>');
+      sinon.assert.calledWith(spy, 'bar');
+      spy.reset();
+
+      expect(swig.render('{% title \'"bar"\'%}')).to.equal('<title>"bar"</title>');
+      sinon.assert.calledWith(spy, '"bar"');
+      spy.reset();
+
+      expect(swig.render('{% title "I\'m title"%}')).to.equal('<title>I\'m title</title>');
+      sinon.assert.calledWith(spy, 'I\'m title');
       spy.reset();
     });
   });
