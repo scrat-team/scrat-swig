e31bbfffda14ec3f99af65d50712b6b0b256b16a
TZ <atian25@qq.com>
2015-05-21 17:35:37 +0800

    require 支持 $group && combo长度限制

--- tags/require.js
+++ tags/require.js
@@ -1,18 +1,24 @@
-
 var parser = require('../lib/parser');
 
 exports.compile = function (compiler, args) {
     var file = null;
+    var group = 0;
     var ctx = [];
-    args.forEach(function(arg){
-        if(arg.key === '$id'){
+    args.forEach(function (arg) {
+        if (arg.key === '$id') {
             file = arg.val;
-        } else {
+        } else if (arg.key === '$group') {
+            group = Number(arg.val);
+            if (isNaN(group)) {
+                throw new Error('Unexpected $group=' + arg.val + ', must be number, default to 0');
+            }
+        }
+        else {
             ctx.push('"' + arg.key + '":' + arg.val);
         }
     });
     ctx = '{' + ctx.join(',') + '}';
-    return ';_output += _ext._resource.include(' + file + ',_utils.extend({},_ctx,' + ctx + '));';
+    return ';_output += _ext._resource.include(' + file + ','  + group + ',_utils.extend({},_ctx,' + ctx + '));';
 };
 exports.parse = parser.parse('$id');
 exports.ends = false;
