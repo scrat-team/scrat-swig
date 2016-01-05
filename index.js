252068a16d5332ca1e79a00fcbf4244fbc9d2487
天猪 <liuyong.ly3@alibaba-inc.com>
2016-01-05 15:32:30 +0800

    feat: add exports

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var swig = require('swig');
+var filters = require('swig/lib/filters');
 var Resource = require('./lib/resource.js');
 
 var tagNames = ['body', 'head', 'html', 'pagelet', 'require', 'script', 'uri', 'title', 'datalet', 'ATF'];
@@ -10,7 +11,12 @@ tagNames.forEach(function extendTags(tag) {
   swig.setTag(tag, t.parse, t.compile, t.ends, t.blockLevel || false);
 });
 
-swig.middleware = function createSwigMiddleware(options) {
+module.exports = exports = swig;
+exports.tagNames = tagNames;
+exports.Resource = Resource;
+exports.filters = exports.filters || filters;
+
+exports.configure = function(options) {
   var map = options.cacheMap ? Resource.loadOptions(options.map) : options.map;
   swig.setExtension('Resource', Resource);
   swig.setExtension('_map', map);
@@ -21,6 +27,10 @@ swig.middleware = function createSwigMiddleware(options) {
   if (options.logger) {
     Resource.setLogger(options.logger);
   }
+};
+
+exports.middleware = function createSwigMiddleware(options) {
+  this.configure(options);
   return function swigMiddleware(req, res, next) {
     var pagelets = req.get('X-Pagelets');
     res.locals._query = req.query;
@@ -35,4 +45,3 @@ swig.middleware = function createSwigMiddleware(options) {
     next();
   };
 };
-module.exports = swig;
